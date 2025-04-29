0357874413f71578b17e28729b42631d6916c3a8
Wjkdj <wlsdnxo0807@gmail.com>
2025-04-29 10:49:24 +0900

    20250429 JWT

--- korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/controller/StudentController.java
+++ korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.example.korea_sleepTech_springboot.controller;
 
+import com.example.korea_sleepTech_springboot.dto.StudentDto;
+import com.example.korea_sleepTech_springboot.entity.B_Student;
 import com.example.korea_sleepTech_springboot.service.StudentService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController // @Controller + @ResponseBody 결합된 애너테이션: RESTful 웹 서비스의 컨트롤러임을 명시
 @RequestMapping("/student") // 해당 컨트롤러의 모든 요청 URL이 "/student"로 시작함을 정의
@@ -16,12 +20,61 @@ public class StudentController {
     }
 
     // 1) 전체 학생 목록 조회 (GET)
+    /*
+    * @Params(요청-매개변수): X
+    * @Return(응답-반환값): List<StudentDto> >> email은 보내지 않음
+    * */
+    // "/student" 경로의 GET 요청을 처리
+    @GetMapping
+    public List<StudentDto> getAllStudents() {
+        return studentService.getAllStudents();
+    }
 
     // 2) 특정 ID로 학생 조회 (GET)
+    /*
+     * @Params(요청-매개변수): id (해당 id로 학생 조회)
+     * @Return(응답-반환값): StudentDto
+     * */
+    // "/student/{id}" 경로로 오는 GET 요청을 처리 - {id}는 경로 변수로 사용
+    @GetMapping("/{id}")
+    public StudentDto getStudentById(@PathVariable Long id) {
+        // @PathVariable: URL 경로에 전달된 동적 데이터를 메서드 파라미터로 매핑
+        return studentService.getStudentById(id);
+    }
 
     // 3) 새로운 학생 등록 (POST)
+    /*
+     * @Params(요청-매개변수): B_Student (학생의 데이터 그대로 가져옴)
+     * @Return(응답-반환값): StudentDto
+     * */
+    @PostMapping
+    public StudentDto createStudent(@RequestBody B_Student student) {
+        // @RequestBody: 클라이언트에서 전달된 JSON 데이터를 B_Student 형태로 변환
+        return studentService.createStudent(student);
+    }
 
     // 4) 특정 ID의 학생 정보 수정 (PUT)
+    /*
+     * @Params(요청-매개변수): 특정 학생의 id, 수정할 학생 정보 StudentDto
+     * @Return(응답-반환값): StudentDto
+     * */
+    @PutMapping
+    public StudentDto updateStudent(@PathVariable Long id, @RequestBody StudentDto studentdto) {
+        return studentService.updateStudent(id, studentdto);
+    }
 
     // 5) 특정 ID의 학생 정보 삭제 (DELETE)
+    /*
+     * @Params(요청-매개변수): 특정 학생의 id
+     * @Return(응답-반환값): ResponseEntity<Void>
+     * */
+    @DeleteMapping
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        
+        // ResponseEntity
+        // : HTTP 응답 전체(= 상태코드, 헤더, 본문)를 직접 제어해서 클라이언트에게 보내는 객체
+        // - 데이터를 보다 안전하게 포장해서 전달
+        return ResponseEntity.noContent().build();
+    }
 }
--- korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/service/StudentService.java
+++ korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/service/StudentService.java
@@ -1,7 +1,127 @@
 package com.example.korea_sleepTech_springboot.service;
 
+import com.example.korea_sleepTech_springboot.dto.StudentDto;
+import com.example.korea_sleepTech_springboot.entity.B_Student;
+import com.example.korea_sleepTech_springboot.repository.StudentRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service // 비즈니스 로직을 처리하는 역할
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<StudentDto> getAllStudents() {
+        // 전체 학생 데이터 조회
+        List<StudentDto> studentDtos = null;
+
+        try {
+            List<B_Student> students = studentRepository.findAll(); // email 까지 포함되어있음
+
+            // email을 포함하지 않는 Dto 형식으로 변환
+            studentDtos = students.stream() // stream API로 데이터 처리 (전체 리스트 순회 + 각 요소에 동일 기능 적용)
+                    .map(student -> new StudentDto (
+                            student.getId(),
+                            student.getName()
+                    ))
+                    .collect(Collectors.toList());
+
+            // 컬렉션 프레임 워크 + stream API
+            // 1) 컬렉션데이터.stream(): 스트림으로 변환
+            // 2) .map(), .filter() 등 중간 연산
+            // 3) 리스트 형태로 다시 변환 - .collect(Collectors.toList());
+
+            return studentDtos;
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, // HTTP 상태 코드
+                    "Error occurred while fetching student", // 에러 메시지
+                    e // 예외 원인
+            );
+        }
+    }
+
+    public StudentDto getStudentById(Long id) {
+        StudentDto studentDto = null;
+
+        try {
+            B_Student student = studentRepository.findById(id)
+                    .orElseThrow(() -> new Error("Student not found with id: " + id));
+
+            studentDto = new StudentDto(
+                    student.getId(),
+                    student.getName()
+            );
+
+            return studentDto;
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, // HTTP 상태 코드
+                    "Error occurred while fetching student", // 에러 메시지
+                    e // 예외 원인
+            );
+        }
+    }
+
+    public StudentDto createStudent(B_Student student) {
+        StudentDto studentDto = null;
+
+        try {
+            B_Student savedStudent = studentRepository.save(student);
+
+            // 저장되고 난 후 B_Student 객체를 DTO로 변환하여 반환
+            studentDto = new StudentDto(
+                    savedStudent.getId(),
+                    student.getName()
+            );
+
+            return studentDto;
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, // HTTP 상태 코드
+                    "Error occurred while fetching student", // 에러 메시지
+                    e // 예외 원인
+            );
+        }
+    }
+
+    public StudentDto updateStudent(Long id, StudentDto studentdto) {
+        StudentDto responseDto = null;
+
+        try {
+            B_Student student = studentRepository.findById(id)
+                    .orElseThrow(() -> new Error("Student not found with id: " + id));
+
+            student.setName(studentdto.getName());
+
+            B_Student updatedStudent = studentRepository.save(student);
+
+            responseDto = new StudentDto(
+                    updatedStudent.getId(),
+                    updatedStudent.getName()
+            );
+
+            return studentdto;
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, // HTTP 상태 코드
+                    "Error occurred while fetching student", // 에러 메시지
+                    e // 예외 원인
+            );
+        }
+    }
+
+    public void deleteStudent(Long id) {
+        
+    }
 }
