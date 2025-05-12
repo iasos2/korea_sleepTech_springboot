e846905112ae74bdea69c732ea8b8c7b3c338d26
Wjkdj <wlsdnxo0807@gmail.com>
2025-05-12 10:46:21 +0900

    20250512 JWT

---
+++ korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.korea_sleepTech_springboot.repository;
+
+import com.example.korea_sleepTech_springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
--- korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/이론/Q_JWT.java
+++ korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/이론/Q_JWT.java
@@ -1,4 +1,11 @@
 package com.example.korea_sleepTech_springboot.이론;
 
 public class Q_JWT {
+    
 }
+/*
+* ===== JWT(JSON Web Token) =====
+* 
+* 1. JWT 란 ?
+*       : JSON 객체를 이용하여 사용자의 인증 정보를 안전하게 전달하는 토큰 기반 인증 방식
+* */
