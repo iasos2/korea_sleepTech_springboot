3d3effea21d14ece6792e34dbf329e2928161596
Wjkdj <wlsdnxo0807@gmail.com>
2025-05-14 09:50:06 +0900

    20250514 JWT

--- korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/service/UserService.java
+++ korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/service/UserService.java
@@ -1,4 +1,8 @@
 package com.example.korea_sleepTech_springboot.service;
 
+import com.example.korea_sleepTech_springboot.dto.response.ResponseDto;
+import com.example.korea_sleepTech_springboot.dto.user.response.GetUserResponseDto;
+
 public interface UserService {
+    ResponseDto<GetUserResponseDto> getUserInfo(String userEmail);
 }
--- korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/service/implementations/UserServiceImpl.java
+++ korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/service/implementations/UserServiceImpl.java
@@ -1,8 +1,31 @@
 package com.example.korea_sleepTech_springboot.service.implementations;
 
+import com.example.korea_sleepTech_springboot.common.ResponseMessage;
+import com.example.korea_sleepTech_springboot.dto.response.ResponseDto;
+import com.example.korea_sleepTech_springboot.dto.user.response.GetUserResponseDto;
+import com.example.korea_sleepTech_springboot.entity.User;
+import com.example.korea_sleepTech_springboot.repository.UserRepository;
 import com.example.korea_sleepTech_springboot.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    // 1) 회원정보 조회
+    @Override
+    public ResponseDto<GetUserResponseDto> getUserInfo(String userEmail) {
+
+        User user = userRepository.findByEmail(userEmail).orElse(null);
+
+        if (user == null) {
+            return ResponseDto.setFailed(ResponseMessage.NOT_EXISTS_USER);
+        }
+
+        GetUserResponseDto data = new GetUserResponseDto(user);
+
+        return ResponseDto.setSuccess(ResponseMessage.SUCCESS, data);
+    }
 }
