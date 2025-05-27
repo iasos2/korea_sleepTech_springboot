5a03f877f5742326dbef75ecee6aced3ea04c2bb
iasos2 <ykkminji@gmail.com>
2025-05-27 12:48:45 +0900

    20250527

---
+++ korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/config/WebFileConfig.java
@@ -0,0 +1,4 @@
+package com.example.korea_sleepTech_springboot.config;
+
+public class WebMvcConfigurer {
+}
---
+++ korea_sleepTech_springboot/src/main/java/com/example/korea_sleepTech_springboot/dto/order/OrderItemRequestDto.java
@@ -0,0 +1,6 @@
+package com.example.korea_sleepTech_springboot.dto.order;
+
+public class OrderItemRequestDto {
+    private Long orderId;
+    private int quantity;
+}
